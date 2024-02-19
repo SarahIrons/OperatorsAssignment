c1314bd5840c56b99b9fcc63ce8abb91256bef36
SarahIrons <sarahirons11@gmail.com>
2024-02-19 15:38:56 -0500

    Add project files.

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName;
+        public string LastName;
+
+        //Create an Employee class with Id, FirstName and LastName properties.
+        //In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.Remember that comparison operators must be overloaded in pairs.
+        public static bool operator ==(Employee employeeA, Employee employeeB)
+        {
+            if (employeeA.Id.Equals(employeeB.Id))
+                return true;
+            else return false;
+        }
+        
+        public static bool operator !=(Employee employeeA, Employee employeeB)
+        {
+            if (employeeA.Id.Equals(employeeB.Id))
+                return false;
+            else return true;
+
+        }
+        
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        public static int Id { get; private set; }
+
+        public static void Main(string[] args)
+        {/* In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their properties.Then compare the two Employee objects using the newly overloaded operators and display the results.*/
+
+            Employee employeeA = new Employee();
+                employeeA.Id = 5004;
+                employeeA.FirstName = "Joe";
+                employeeA.LastName = "Jones";
+
+            Employee employeeB = new Employee();
+               employeeB.Id = 5004;
+                employeeB.FirstName = "Joe";
+               employeeB.LastName = "James";
+
+            checkId(employeeA, employeeB);
+        
+
+       static void checkId(Employee employeeA, Employee employeeB)
+        
+                {
+                    if (employeeA != employeeB)
+                 
+                    {
+                        Console.WriteLine("Everything checks out and your Id number is valid.");
+                    }
+                    if (employeeA == employeeB)
+                    {
+                        Console.WriteLine("There is an error and you have a duplicate employee Id number.");
+                    }
+                }
+            }
+          
+        
+    }
+}
